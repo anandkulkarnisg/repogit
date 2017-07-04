964089724a68ca2e4a193625ba22dd396398df40
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2017-07-04 13:23:47 +0800

    Added minor changes to the file for resetting the array after each sorting algorithm.

--- cpp/sorting/SortingAlgorithms.cpp
+++ cpp/sorting/SortingAlgorithms.cpp
@@ -26,15 +26,16 @@ int main(int argc, char* argv[]) {
 	for (std::size_t it = 0; it < totalsize; ++it) {
 		integerSorter.setSortingMode(SortingMode::descending);
 		cout << "running " << sortingNames[it] << " sorting" << endl;
-		cout << "before :" << endl;
+		cout << "before:-" << endl;
 		integerSorter.printArray();
 		ptr[it](integerSorter);
-		cout << "after :" << endl;
+		cout << "after descending sort:-" << endl;
 		integerSorter.printArray();
 		integerSorter.setSortingMode(SortingMode::ascending);
 		ptr[it](integerSorter);
-		cout << "after :" << endl;
+		cout << "after ascending sort:-" << endl;
 		integerSorter.printArray();
+		arr[0] = 5; arr[1]=2 ; arr[2]=3; arr[3]=1; arr[4]=4;
 	}
 	return (0);
 
