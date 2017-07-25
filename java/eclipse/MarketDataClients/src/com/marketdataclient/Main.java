9843a52410390e7b1da62845befd4182d4e944d7
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2017-07-25 21:31:29 +0800

    Anand Kulkarni - Added proper switch for Publisher types using enum.

--- java/eclipse/MarketDataClients/src/com/marketdataclient/Main.java
+++ java/eclipse/MarketDataClients/src/com/marketdataclient/Main.java
@@ -10,37 +10,45 @@ public class Main
 {
 	final static Logger logger = LogManager.getLogger(Main.class);
 
+	private enum publisherType
+	{
+		ICICI, CNBC;
+	}
+
 	public static void main(String[] args)
 	{
 		// Set a default publisher Type.
-		String publisherType = "icici";
+		publisherType publisher = publisherType.ICICI;
+
 		if (args.length > 0)
-			publisherType = args[0];
+			publisher = publisherType.valueOf(args[0].toUpperCase());
 
 		// Kick Start the application with initial log message.
-		logger.info("Starting the MarketDataClient Application.");
+		logger.info("Starting the MarketDataClient Application with publisherType = " + publisher.toString());
 
 		// Load the properties Configuration file for the marketDataConfig.
-		MarketDataConfigManager marketDataConfig = new MarketDataConfigManager(publisherType);
+		MarketDataConfigManager marketDataConfig = new MarketDataConfigManager(publisher.toString());
 
 		// Load the stock symbols required for the WorkerManager.
 		String[] symbolList = marketDataConfig.loadEquitySymbolsFromConfigFile();
 
-		// Invoke the CNBC Worker Manager now.
-		if (publisherType.equalsIgnoreCase("cnbc"))
+		switch (publisher)
 		{
-			CNBCWorkerManager cnbcWorkerManager = new CNBCWorkerManager(marketDataConfig, symbolList);
-			cnbcWorkerManager.start();
-			System.exit(0);
-		}
-
-		// Kick start the ICICI Worker Manager instance.
-		if (publisherType.equalsIgnoreCase("icici"))
-		{
+		case ICICI:
 			ICICIWorkerManager iciciWorkerManager = new ICICIWorkerManager(marketDataConfig, symbolList);
 			iciciWorkerManager.start();
-			System.exit(0);
+			break;
+
+		case CNBC:
+			CNBCWorkerManager cnbcWorkerManager = new CNBCWorkerManager(marketDataConfig, symbolList);
+			cnbcWorkerManager.start();
+			break;
+
+		default:
+			logger.info("unknown publisherType = " + publisher.toString() + ". Exiting the application.");
+
 		}
+
 		System.exit(0);
 	}
 }
