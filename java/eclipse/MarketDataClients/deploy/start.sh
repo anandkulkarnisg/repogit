6cc65edb230f0173bb96629b65806d06696e6c6c
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2017-07-25 17:36:18 +0800

    Anand Kulkarni - 25/07/2017 - Added improved scripts.

--- java/eclipse/MarketDataClients/deploy/start.sh
+++ java/eclipse/MarketDataClients/deploy/start.sh
@@ -7,6 +7,6 @@ if [ $cwd != "/home/anand/repogit/java/eclipse/MarketDataClients/deploy" ]; then
 	cwd=$PWD
 fi
 
-nohup ~/runq5000 $cwd/kdb/start.q &
-nohup java -jar $cwd/MarketDataClient.jar icici &
+nohup ~/runq5000 $cwd/kdb/$1start.q &
+nohup java -jar $cwd/MarketDataClient.jar $1 &
 cd $backupcwd
